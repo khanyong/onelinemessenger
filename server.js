445722b5c1d66de9ab80a1ab90b7b784d55c1b97
khanyong <khanyong.yoo@gmail.com>
2023-05-16 19:01:35 +0900

    fourth

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = 3000;
+
+// MongoDB 연결 URL 및 옵션
+const url = 'mongodb://localhost:27017';
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // 미들웨어 설정
 app.use(express.json());
 
-// API 엔드포인트와 요청 처리
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// MongoDB 연결 생성
+MongoClient.connect(url, options, (err, client) => {
+  if (err) {
+    console.error('Failed to connect to MongoDB:', err);
+  } else {
+    console.log('Connected to MongoDB');
+
+    // 데이터베이스 및 컬렉션 선택
+    const db = client.db('mydatabase');
+    const collection = db.collection('messages');
+
+    // API 엔드포인트와 요청 처리
+    app.get('/', (req, res) => {
+      collection.find().toArray((err, messages) => {
+        if (err) {
+          console.error('Failed to fetch messages from MongoDB:', err);
+          res.status(500).send('Failed to fetch messages');
+        } else {
+          res.json(messages);
+        }
+      });
+    });
+  }
 });
 
 // 서버 실행
